63d7a7ff71c83dfed6412cb536ac850964a489bd
mmessmer <mmessmer@redhat.com>
2023-09-06 16:28:39 -0500

    updates to design principles

--- src/assets/js/design-principles.js
+++ src/assets/js/design-principles.js
@@ -1,52 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const expandIcons = document.querySelectorAll(".expand-icon");
     const shrinkIcons = document.querySelectorAll(".shrink-icon");
     let scrollPosition = 0;
     let previousActiveElement = null;
-    function handleExpand(icon) {
-        const targetId = icon.getAttribute('data-target');
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            if (targetElement.classList.contains('d-none')) {
+    const dpBlocks = document.querySelectorAll('.dp-blocks__block');
+    function handleExpand(block) {
+        const sectionId = block.getAttribute('aria-controls');
+        const targetSection = document.getElementById(sectionId);
+        if (targetSection) {
+            const isHidden = targetSection.getAttribute('aria-hidden') === 'true';
+            targetSection.setAttribute('aria-hidden', !isHidden);
+            if (targetSection.classList.contains('d-none')) {
                 scrollPosition = window.scrollY;
-                previousActiveElement = document.activeElement;
+                window.scrollTo({ top: 0, behavior: 'auto' });
+                targetSection.classList.remove('d-none');
                 document.getElementById("default-blocks").classList.add("d-none");
                 document.getElementById("design-principles-hero").classList.add("d-none");
-                targetElement.classList.remove('d-none');
-                window.scrollTo({ top: 0, behavior: 'auto' });
+                previousActiveElement = document.activeElement;
+            }
+            block.setAttribute('aria-expanded', true);
+        }
+    }
+    function handleClose(targetSection) {
+        if (targetSection) {
+            if (!targetSection.classList.contains('d-none')) {
+                targetSection.classList.add('d-none');
+                document.getElementById("default-blocks").classList.remove("d-none");
+                document.getElementById("design-principles-hero").classList.remove("d-none");
+                window.scrollTo(0, scrollPosition);
+                if (previousActiveElement) {
+                    previousActiveElement.focus();
+                    previousActiveElement.blur();
+                }
+                document.querySelector(`button[aria-controls="${targetSection.id}"]`).setAttribute('aria-expanded', 'false');
+                document.getElementById(`${targetSection.id}`).setAttribute('aria-hidden', 'true');
             }
         }
     }
-    function handleClose() {
-        const expandedBlocks = document.querySelectorAll(".expanded-block");
-        expandedBlocks.forEach(block => {
-            document.getElementById("default-blocks").classList.remove("d-none");
-            document.getElementById("design-principles-hero").classList.remove("d-none");
-            if ( !block.classList.contains("d-none") ) {
-                block.classList.add("d-none");
-            }
+    dpBlocks.forEach((block) => {
+        block.addEventListener('click', () => {
+            handleExpand(block);
         });
-        // restore the scroll position
-        window.scrollTo(0, scrollPosition);
-        // set focus back to the previously focused element
-        if (previousActiveElement) {
-            previousActiveElement.focus();
-            previousActiveElement.blur();
-        }
-    }
-    expandIcons.forEach(icon => {
-        icon.addEventListener("click", () => {
-            handleExpand(icon);
-        });
-        icon.addEventListener("keyup", function(e) {
+        block.addEventListener("keyup", function(e) {
             if (e.keyCode === 13) {
                 this.click();
             }
         });
-    });
+    });    
     shrinkIcons.forEach(icon => {
         icon.addEventListener("click", () => {
-            handleClose();
+            const targetId = icon.getAttribute('aria-controls');
+            const targetSection = document.getElementById(targetId);
+            handleClose(targetSection);
         });
         icon.addEventListener("keyup", function(e) {
             if (e.keyCode === 13) {
